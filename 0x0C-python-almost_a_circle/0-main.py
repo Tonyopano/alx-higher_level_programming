780606f662390177a5154217d980a76494d1e736
Tonyopano <antonyopano@gmail.com>
2022-11-04 01:27:31 -0700

    adding main file

---
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
